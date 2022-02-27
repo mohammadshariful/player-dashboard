e4578fa352d37003a4b8d2b929037edeffbacc18
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-02-27 22:14:32 +0600

    change and modify

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const loadData = async () => {
   const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputText}`;
   const response = await fetch(url);
   const data = await response.json();
-  if (data.player) {
+  if (data.player === null || inputText == "") {
     showElement("loading-sipnner", "none");
     showElement("error-msg", "block");
   } else {
@@ -45,14 +45,15 @@ const displayData = (players) => {
            <img src="${strThumb}" alt="" />
           </div>
                </div>
-                <div class="card-body text-center mb-3">
+                <div class="card-body text-center">
                     <h5 class="card-title mb-2">${strPlayer}</h5>
                     <p class="card-text">
                      ${dateBorn}
                     </p>
                     <div class="my-2">
                       <button class="btn btn-danger p-1 delete-btn">Delete</button>
-                      <button class="btn btn-success p-1" onclick ="seeDetails(${idPlayer})">Details</button>
+                      <button class="btn btn-success p-1" onclick ="seeDetails(${idPlayer})"  data-bs-toggle="modal"
+                      data-bs-target="#exampleModal">Details</button>
                     </div>
                     <!-- icons -->
                     <div class="text-primary">
@@ -83,6 +84,56 @@ container.addEventListener("click", function (e) {
     e.target.parentNode.parentNode.parentNode.remove();
   }
 });
-const seeDetails = (playerId) => {
-  console.log(playerId);
+const seeDetails = async (playerId) => {
+  const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${playerId}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  showInfo(data.players[0]);
+  console.log(data.players[0]);
+};
+
+const showInfo = (data) => {
+  const {
+    idPlayer,
+    strPlayer,
+    strDescriptionEN,
+    dateBorn,
+    strThumb,
+    strGender,
+    strHeight,
+    strWeight,
+    strNationality,
+    strSport,
+    strTeam,
+  } = data;
+  const modalBody = document.getElementById("modal-body");
+  modalBody.innerHTML = `
+   <div class="card">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="${strThumb}" class="img-fluid  rounded-start" alt="..." />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">${strPlayer}</h5>
+                    <div class="card-text">
+                     <p>Player ID : ${idPlayer}</p>
+                     <p>BOD: ${dateBorn}</p>
+                     <p>Gender: ${strGender}</p>
+                     <p>Height: ${strHeight}</p>
+                     <p>Weight: ${strWeight}</p>
+                     <p>Sport: ${strSport}</p>
+                     <p>Team: ${strTeam}</p>
+                     <p>Nationality: ${strNationality}</p>
+                    </div>
+                    <p class="card-text">
+                      <small class="text-muted">
+                      ${strDescriptionEN.slice(0, 100)}
+                      </small>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+  `;
 };
